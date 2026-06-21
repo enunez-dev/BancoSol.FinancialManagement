2447147846c184c5313d61c4b0461ebd00d03f0e
Edward Nuñez <enunez.dev@email.com>
2026-06-21 08:09:13 -0400

    Add browser-like headers to exchange rate HTTP client

--- Infrastructure/DependencyInjection.cs
+++ Infrastructure/DependencyInjection.cs
@@ -1,3 +1,4 @@
+using System.Net.Http.Headers;
 using Application.Abstractions;
 using Infrastructure.ExchangeRates;
 using Microsoft.Extensions.Configuration;
@@ -15,6 +16,13 @@ public static class DependencyInjection
         services.AddHttpClient<IExchangeRateProvider, HexaRateExchangeRateProvider>(httpClient =>
         {
             httpClient.BaseAddress = new Uri(hexaRateOptions.BaseUrl);
+            httpClient.DefaultRequestHeaders.Accept.Clear();
+            httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("text/plain"));
+            httpClient.DefaultRequestHeaders.AcceptLanguage.Clear();
+            httpClient.DefaultRequestHeaders.AcceptLanguage.ParseAdd("en-US,en;q=0.9");
+            httpClient.DefaultRequestHeaders.UserAgent.ParseAdd("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/137.0.0.0 Safari/537.36");
+            httpClient.DefaultRequestHeaders.Referrer = new Uri("https://hexarate.paikama.co/");
         });
 
         return services;
